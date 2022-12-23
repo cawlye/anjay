e65a979fa5395b9ecb3498e17ef66eb8a8194bc9
cawlye <113624168+cawlye@users.noreply.github.com>
2022-12-23 00:07:29 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8000
 server.listen(PORT, function() {
   if (server.listening) {
     console.log(`Server is listening on port ${PORT}`);
